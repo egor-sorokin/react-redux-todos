4a71738219fb64d0ba604e8ef283e07aaf6149ea
Egor Sorokin <egorsorokin@Handsomes-MacBook-Pro.local>
2017-08-24 22:03:10 +0600

    Add route for app

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,10 +1,16 @@
 import React, { PropTypes } from 'react';
 import { Provider } from 'react-redux';
 import App from './App';
+import { Router, Route } from 'react-router';
+import createBrowserHistory from 'history/createBrowserHistory';
+
+const customHistory = createBrowserHistory();
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <App />
+    <Router history={customHistory}>
+      <Route path="/" component={App} />
+    </Router>
   </Provider>
 );
 
