4a2c7e2a494d38b6df4c4015ce10f0da390dd358
Egor Sorokin <egorsorokin@Handsomes-MacBook-Pro.local>
2017-08-28 09:51:42 +0600

    Add applyMiddleware and add logger and promise packages

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,55 +1,23 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import todoApp from './reducers';
+import promise from 'redux-promise';
+import createLogger from 'redux-logger';
 
-
-const logger = (store) => (rawDispatchNext) => {
-  /* eslint-disable no-console */
-  if (!console.group) {
-    return rawDispatchNext;
-  }
-
-  return (action) => {
-    console.group(action.type);
-    console.log('%c prev state:', 'color: black', store.getState());
-    console.log('%c action:', 'color: red', action);
-    const returnValue = rawDispatchNext(action);
-    console.log('%c next state:', 'color: green', store.getState());
-    console.groupEnd(action.type);
-
-    return returnValue;
-  };
-  /* eslint-enable no-console */
-};
-
-
-const promise = (store) => (rawDispatchNext) => (action) => { // eslint-disable-line no-unused-vars
-  if (typeof action.then === 'function') {
-    return action.then(rawDispatchNext);
-  }
-
-  return rawDispatchNext(action);
-};
-
-
-const wrapDispatchWithMiddlewares = (store, middlewares) =>
-  middlewares.slice().reverse().forEach(middleware => {
-    store.dispatch = middleware(store)(store.dispatch); // eslint-disable-line no-param-reassign
-  });
 
 const configureStore = () => {
-  const store = createStore(todoApp);
   // middlewares collect all dispatch overriding functions
   // + store.dispatch = addPromiseSupportToDispatch(store);
   const middlewares = [promise];
 
   if (process.env.NODE_ENV !== 'production') {
     // store.dispatch = addLoggingToDispatch(store);
-    middlewares.push(logger);
+    middlewares.push(createLogger());
   }
 
-
-  wrapDispatchWithMiddlewares(store, middlewares);
-  return store;
+  return store = createStore(
+    todoApp,
+    applyMiddleware(...middlewares)
+  );
 };
 
 export default configureStore;
