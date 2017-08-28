ba4c83abe99bd18ec6ea5d6f6e1cd6f3f7b06392
Egor Sorokin <egorsorokin@Handsomes-MacBook-Pro.local>
2017-08-28 11:27:31 +0600

    Update the state with fetched data

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import todoApp from './reducers';
 import promise from 'redux-promise';
-import { createLogger } from 'redux-logger'
+import { createLogger } from 'redux-logger';
 
 
 const configureStore = () => {
