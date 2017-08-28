f3150e40da428960e6fa8e5aaf6cd9525f808bbd
Egor Sorokin <egorsorokin@Handsomes-MacBook-Pro.local>
2017-08-28 09:42:32 +0600

    Add middleware chain

--- src/configureStore.js
+++ src/configureStore.js
@@ -2,45 +2,53 @@ import { createStore } from 'redux';
 import todoApp from './reducers';
 
 
-const addLoggingToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
-
+const logger = (store) => (rawDispatchNext) => {
+  /* eslint-disable no-console */
   if (!console.group) {
-    return rawDispatch;
+    return rawDispatchNext;
   }
 
   return (action) => {
     console.group(action.type);
     console.log('%c prev state:', 'color: black', store.getState());
     console.log('%c action:', 'color: red', action);
-    const returnValue = rawDispatch(action);
+    const returnValue = rawDispatchNext(action);
     console.log('%c next state:', 'color: green', store.getState());
     console.groupEnd(action.type);
 
     return returnValue;
   };
+  /* eslint-enable no-console */
 };
 
-const addPromiseSupportToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
 
-  return (action) => {
-    if (typeof action.then === 'function') {
-      return action.then(rawDispatch);
-    }
+const promise = (store) => (rawDispatchNext) => (action) => { // eslint-disable-line no-unused-vars
+  if (typeof action.then === 'function') {
+    return action.then(rawDispatchNext);
+  }
 
-    return rawDispatch(action);
-  };
+  return rawDispatchNext(action);
 };
+
+
+const wrapDispatchWithMiddlewares = (store, middlewares) =>
+  middlewares.slice().reverse().forEach(middleware => {
+    store.dispatch = middleware(store)(store.dispatch); // eslint-disable-line no-param-reassign
+  });
 
 const configureStore = () => {
   const store = createStore(todoApp);
+  // middlewares collect all dispatch overriding functions
+  // + store.dispatch = addPromiseSupportToDispatch(store);
+  const middlewares = [promise];
 
   if (process.env.NODE_ENV !== 'production') {
-    store.dispatch = addLoggingToDispatch(store);
+    // store.dispatch = addLoggingToDispatch(store);
+    middlewares.push(logger);
   }
 
-  store.dispatch = addPromiseSupportToDispatch(store);
+
+  wrapDispatchWithMiddlewares(store, middlewares);
   return store;
 };
 
