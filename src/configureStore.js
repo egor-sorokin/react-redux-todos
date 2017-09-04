154cc95d2af9038eea1b8ce3d17ac15033927f90
Egor Sorokin <egorsorokin@Handsomes-MacBook-Pro.local>
2017-09-04 21:09:42 +0600

    Add redux-thunk middleware

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,13 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import todoApp from './reducers';
 import { createLogger } from 'redux-logger';
-
-
-const thunk = (store) => (next) => (action) => // eslint-disable-line no-confusing-arrow
-  typeof action === 'function' ?
-    action(store.dispatch) :
-    next(action);
-
 
 const configureStore = () => {
   // middlewares collect all dispatch overriding functions
